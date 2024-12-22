eaccfb48a6ec57fd49e30eb09ca1b89debe2a5d6
zunairanureen <zunairanoreen127@gmail.com>
2024-12-22 06:11:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData
 from src.pipeline.predict_pipeline import PredictPipeline
 
-
 application=Flask(__name__)
 
 app=application
